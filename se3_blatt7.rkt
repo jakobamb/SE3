88cd171deaee6bc034667705de27b38c0a05f166
Jakob Ambsdorf <jakob.ambsdorf@gmail.com>
2017-12-17 16:47:13 +0100

    anfang aufgabe 2

--- se3_blatt7.rkt
+++ se3_blatt7.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+(require 2htdp/image)
 (require racket/trace)
 
 ;Aufgabe 1
@@ -33,3 +34,41 @@
 (zaehlen 3 (list 2 5 7 3 56 3 6456 3 543 42))
 (zaehlen_end 3 (list 2 5 7 3 56 3 6456 3 543 42))
 (zaehlen_ho 3 (list 2 5 7 3 56 3 6456 3 543 42))
+
+
+;Aufgabe 2
+
+#|
+2.1
+
+Als Datenstruktur nutzen wir eine Liste von N (mit N=30) Listen, die jeweils eine Spalten des Spielfeldes repräsentiert.
+Jede dieser Spalten enthält N Elemente, die durch einen Wahrheitswert anzeigen, ob ein Feld lebendig (#t) oder tot (#f) ist.
+
+Durch die Unterteilung in Spalten kann man relativ einfach benachbarte Zellen finden, ansonsten ist die Datenstruktur auf das wesentliche reduziert,
+sodass sie die in den Folgeaufgaben benötigten Operationen gut unterstützt.
+
+TODO: Hier noch ein paar Zeilen wenn die Funktionen implementiert sind.
+|#
+
+;2.2
+
+(define (draw-row cells xoffset yoffset scene)
+  (if (null? cells)
+      scene
+      (place-image (square 20
+                           (if (car cells)
+                                  ;ausfüllen wenn lebendig, sonst nur umranden
+                                  "solid"
+                                  "outline")
+                           "black")
+                   xoffset
+                   yoffset
+                   (draw-row (cdr cells) (+ xoffset 20) yoffset scene))))
+
+(define (draw-board state)
+  (foldr (lambda (acc row)
+           (draw-row 
+
+(draw-row (list #t #t #f #f #f #t #f #f #f #t #t) 0 10 (empty-scene 800 600))
+                           
+
